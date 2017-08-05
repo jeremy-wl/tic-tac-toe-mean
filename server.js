cc0d4817fb601b564473aeb37f40859fd8228cab
Wenlin Huang <jwong8593@gmail.com>
2017-08-04 19:36:45 -0700

    Make sure player leaves current room before creating/joining new rooms

--- server.js
+++ server.js
@@ -36,46 +36,48 @@ var socketQueue = []
 
 io.on('connection', function (socket) {
 
-    function createRoom(socket) {
-        var room = socket.id
-        socketQueue.push(room)
-    }
-
     socket.on('create room', function (username) {
-        createRoom(socket)
-        console.log('Player (socket id: ' + socket.id + ') named ' + username + ' joins ' + socket.id)
+        // already the only one in the room
+
+        var room = socket.room
+        if (room) {
+            var clients = socket.adapter.rooms[room].sockets
+            if (Object.keys(clients).length === 1)  return
+        }
+        createRoom(socket, username)
     })
 
     socket.on('join room', function (username) {
-        for (var i in socketQueue) {
+        var flag = false  // there is only 1 room with 1 client, and that room is created by me
+        for (var i = 0; i < socketQueue.length; i++) {
             var room = socketQueue[i]
-            var clients = io.sockets.adapter.rooms[room].sockets
+            var clients = socket.adapter.rooms[room].sockets
             var numClients = (typeof clients !== 'undefined') ? Object.keys(clients).length : 0
             if (numClients === 1) {
-                socket.join(room)
-                console.log('Player (socket id: ' + socket.id + ') named ' + username + ' joins ' + room)
+                if (room === socket.id) {
+                    flag = true
+                    continue
+                }
+
+                leaveRoom(socket, username)
+                joinRoom(socket, username, room)
+
                 io.to(room).emit('join room', username)
                 return
             }
         }
+        if (flag) return
         // no room to join, then create one
-        createRoom(socket)
+        createRoom(socket, username)
     })
 
     socket.on('share initial data', function (data) {
-        var room = socket.id
-        io.to(room).emit('share initial data', data)
-    })
 
-    socket.on('game start', function (data) {
-        var s = socketQueue.find(function (room) {
-            var clients = io.sockets.adapter.rooms[room].sockets
-            for (var i in clients) {
-                return Object.keys(clients[i] === socket.id)
-            }
-        })
-        var index = socketQueue.indexOf(s)
-        socketQueue.splice(index, 1)
+        removeRoomFromQueue(socket)
+
+        var room = socket.id
+
+        io.to(room).emit('share initial data', data)
     })
 
     socket.on('move made', function(moveObj){
@@ -83,11 +85,48 @@ io.on('connection', function (socket) {
         io.to(room).emit('move made', moveObj)
     });
 
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+    socket.on('disconnect', function(socket) {
+        console.log('Player (socket id: '+ socket.id + ') disconnects');
         // remove client from room in the queue (automatically?)
     });
 
+
+    function createRoom(socket, username) {
+        leaveRoom(socket, username)
+
+        var room = socket.id
+        socketQueue.push(room)
+        console.log('new room ' + socket.id + ' created by' + username)
+
+        joinRoom(socket, username, room)
+    }
+
+    function leaveRoom(socket, username) {
+        if (socket.room) {
+            console.log('Player ' + username + '                      (' + socket.id + ') leaves ' + socket.room)
+        }
+        socket.leave(socket.room)
+        socket.room = null
+    }
+
+    function joinRoom(socket, username, room) {
+        if (!socket.room) {
+            console.log('Player ' + username + '                      (' + socket.id + ') joins ' + room)
+        }
+        socket.join(room)
+        socket.room = room
+    }
+
+    function removeRoomFromQueue(socket) {
+        var s = socketQueue.find(function (room) {
+            var clients = socket.adapter.rooms[room].sockets
+            for (var i in clients) {
+                return Object.keys(clients[i] === socket.id)
+            }
+        })
+        var index = socketQueue.indexOf(s)
+        socketQueue.splice(index, 1)
+    }
 
 })
 
