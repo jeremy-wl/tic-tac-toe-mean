6f4173541f18895f833407555016e40cfae71997
Wenlin Huang <jwong8593@gmail.com>
2017-08-12 01:32:59 -0700

    Added mlab configurations

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose')
 
 var connectionString = 'mongodb://localhost/tic-tac-toe' // for local
-// if (process.env.MLAB_USERNAME_WEBDEV) {              // check if running remotely
-//     var username = process.env.MLAB_USERNAME_WEBDEV  // get from environment
-//     var password = process.env.MLAB_PASSWORD_WEBDEV
-//     connectionString = 'mongodb://' + username + ':' + password
-//     connectionString += '@ds147821.mlab.com:47821/heroku_sccp2r5d'
-// }
+if (process.env.MLAB_USERNAME_WEBDEV) {              // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV  // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV
+    connectionString = 'mongodb://' + username + ':' + password
+    connectionString += '@ds043200.mlab.com:43200/heroku_mkcqdh44'
+}
 
 mongoose.connect(connectionString, { useMongoClient: true })
 mongoose.Promise = require('q').Promise
